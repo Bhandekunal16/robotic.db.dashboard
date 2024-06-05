a82f35b5417b8b7e739a8543e33b85a2351db84a
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 13:49:21 +0530

    "Added HomeComponent to app routes and updated children array in AuthComponent route"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,15 @@
 import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   {
     component: AuthComponent,
     path: '',
-    children: [{ component: ContactComponent, path: 'contact' }],
+    children: [
+      { component: ContactComponent, path: 'contact' },
+      { component: HomeComponent, path: 'home' },
+    ],
   },
 ];
