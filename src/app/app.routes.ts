eed8a67df6c5bc8107fdae2a52851956e9698af3
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 13:53:01 +0530

    "Added AboutComponent to app routes"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { component: ContactComponent, path: 'contact' },
       { component: HomeComponent, path: 'home' },
+      { component: AboutComponent, path: 'about' },
     ],
   },
 ];
