50c88324621437e6d574050be5f6dd01c7211074
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 14:29:11 +0530

    "Updated HomeComponent to display package information, added HTTP request to fetch data, and styled the component with SCSS."

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  public name: string | undefined;
+  public createdOn: string | undefined;
+  public modifiedOn: string | undefined;
+  public author: string | undefined;
+  public description: string | undefined;
+  public maintainers: string | undefined;
+  public license: string | undefined;
 
+  ngOnInit(): void {
+    this.info().subscribe((ele) => {
+      console.log(ele);
+      this.name = ele.data.name;
+      this.createdOn = ele.data.time.created;
+      this.modifiedOn = ele.data.time.modified;
+      this.author = ele.data.author.name;
+      this.description = ele.data.description;
+      this.maintainers = ele.data.maintainers[0].name;
+      this.license = ele.data.license;
+    });
+  }
+
+  info(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>(
+        'https://townhall-ten.vercel.app/info',
+        { name: 'roboticdb' },
+        { headers }
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
