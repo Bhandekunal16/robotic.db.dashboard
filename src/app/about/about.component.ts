49667f4a33f2cc16e06958069d0769b854ec7336
bhandekunal16 <bhandekunal16@gmail.com>
2024-06-05 16:13:30 +0530

    "Updated AboutComponent to display package information, added HTTP request to fetch data"

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  HttpClient,
+  HttpClientModule,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.scss'
+  styleUrl: './about.component.scss',
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  public data: any[] | undefined;
 
+  ngOnInit(): void {
+    this.info().subscribe((ele) => {
+      const obj = ele.data.versions;
+      const arr = [];
+      for (let key in obj) {
+        arr.push({ key: key, value: obj[key] });
+      }
+      console.log(arr);
+
+      this.data = arr;
+    });
+  }
+
+  info(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+    });
+
+    return this.http
+      .post<any>(
+        'https://townhall-ten.vercel.app/info',
+        { name: 'roboticdb' },
+        { headers }
+      )
+      .pipe(
+        catchError((error) => {
+          return throwError(error);
+        })
+      );
+  }
 }
